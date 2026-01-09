e8352c208359668eddab0bf0bdedb27b0487313f
Anthony San Lucas <asanlucas@naturisa.com.ec>
2026-01-08 23:16:49 -0500

    Update Astro dependency and change CNAME for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://calculadoraug.live',
+  site: 'https://calculadoraug.asanlucas.com',
   integrations: [sitemap()],
 });
